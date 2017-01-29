e4fbc44e5597aade42ab3be867f8ac7079b9fcdb
Martin Gabelmann <martin@gabelmann.biz>
2017-01-29 16:57:03 +0100

    fix layouts (now a awful thing)

--- config.lua
+++ config.lua
@@ -74,7 +74,7 @@ wp_aspectratio = true
 -- }}}
 
 -- {{{ Table of layouts to cover with awful.layout.inc, order matters.
-layouts = {
+awful.layout.layouts = {
     awful.layout.suit.tile,
     awful.layout.suit.tile.left,
     awful.layout.suit.tile.bottom,
@@ -82,9 +82,7 @@ layouts = {
     awful.layout.suit.fair,
     awful.layout.suit.fair.horizontal,
     awful.layout.suit.max,
-    awful.layout.suit.max.fullscreen,
-    awful.layout.suit.magnifier,
-    awful.layout.suit.corner.nw,
+    awful.layout.suit.corner.nw
 }
 
 -- }}}
