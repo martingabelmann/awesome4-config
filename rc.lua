fd5f8d4086ad1e896688927b5945de59eea71e72
Martin Gabelmann <martin@gabelmann.biz>
2017-02-16 18:45:34 +0100

    use mod+q for killing too

--- rc.lua
+++ rc.lua
@@ -392,6 +392,8 @@ clientkeys = awful.util.table.join(
             c:raise()
         end,
         {description = "toggle fullscreen", group = "client"}),
+    awful.key({ modkey            }, "q",      function (c) c:kill()                         end,
+              {description = "close", group = "client"}),
     awful.key({ modkey, "Shift"   }, "c",      function (c) c:kill()                         end,
               {description = "close", group = "client"}),
     awful.key({ modkey, "Control" }, "space",  awful.client.floating.toggle                     ,
